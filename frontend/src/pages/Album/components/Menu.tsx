ec7dc34650330d160cd7911a1ea4275d23d7b2eb
Bíró Kálmán <linkee12@gmail.com>
2025-08-22 14:21:31 +0200

    portrait buttons slide bug

--- frontend/src/pages/Album/components/Menu.tsx
+++ frontend/src/pages/Album/components/Menu.tsx
@@ -1,7 +1,11 @@
 import landscapeButtonsBg from "@assets/images/landscapeButtonsBg.svg?no-inline";
+import albumItemsBg from "@assets/images/albumItemsBg.svg?no-inline";
 import LandscapeDownloadIcon from "@assets/images/icons/landscapeDownloadIcon.svg?react";
+import SlideUp from "@assets/images/icons/slideUp.svg?react";
+import SlideDown from "@assets/images/icons/slideDown.svg?react";
 import LandscapeAddIcon from "@assets/images/icons/landscapeAddIcon.svg?react";
 import { styled } from "../../../stitches.config";
+import { useEffect, useState } from "react";
 
 type MenuProps = {
   onDownloadAll: () => void;
@@ -10,31 +14,59 @@ type MenuProps = {
 };
 
 export function Menu(props: MenuProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => setShowButton(true), 400);
+    } else {
+      setShowButton(false);
+    }
+  }, [isOpen]);
+
   return (
-    <LandscapeButtonsBg>
-      <LandscapeButton disabled={props.isBusy}>
-        <ButtonText onClick={props.onDownloadAll}>DOWNLOAD ALL</ButtonText>
-        <LandscapeDownloadIcon />
-      </LandscapeButton>
-      <LandscapeButton>
-        <ButtonText onClick={props.onAddPhoto}> ADD PHOTO</ButtonText>
-        <LandscapeAddIcon />
-      </LandscapeButton>
-    </LandscapeButtonsBg>
+    <>
+      <LandscapeButtonsBg>
+        <Button disabled={props.isBusy}>
+          <ButtonText onClick={props.onDownloadAll}>DOWNLOAD ALL</ButtonText>
+          <LandscapeDownloadIcon />
+        </Button>
+        <Button>
+          <ButtonText onClick={props.onAddPhoto}> ADD PHOTO</ButtonText>
+          <LandscapeAddIcon />
+        </Button>
+      </LandscapeButtonsBg>
+      <PortraitButtonsContainer onClick={() => setIsOpen(!isOpen)}>
+        <PortraitButtonsHeader isOpen={isOpen} />
+        <Bottom isOpen={isOpen}>
+          <SlideUp />
+          <Button disabled={props.isBusy} isOpen={showButton && isOpen}>
+            <div></div>
+            <ButtonText onClick={props.onDownloadAll}>DOWNLOAD ALL</ButtonText>
+            <LandscapeDownloadIcon />
+          </Button>
+          <Button isOpen={showButton && isOpen}>
+            <div></div>
+            <ButtonText onClick={props.onAddPhoto}> ADD PHOTO</ButtonText>
+            <LandscapeAddIcon />
+          </Button>
+        </Bottom>
+      </PortraitButtonsContainer>
+    </>
   );
 }
 
 const LandscapeButtonsBg = styled("div", {
-  display: "flex",
+  "@portrait": { display: "none" },
+  "@landscape": { display: "flex" },
+  maskRepeat: "no-repeat",
+  backgroundSize: "100% 100%",
   height: "5rem",
   justifyContent: "right",
   alignItems: "center",
   maskSize: "cover",
   backgroundColor: "#0E0E0E",
   maskImage: `url(${landscapeButtonsBg})`,
-  maskRepeat: "no-repeat",
-  backgroundSize: "100% 100%",
-  transition: "display 0.3s",
   variants: {
     show: {
       true: {
@@ -45,11 +77,65 @@ const LandscapeButtonsBg = styled("div", {
   },
 });
 
-const LandscapeButton = styled("button", {
+const PortraitButtonsContainer = styled("div", {
+  "@portrait": { display: "flex" },
+  "@landscape": { display: "none" },
+  cursor: "pointer",
+  flexDirection: "column",
+  transition: "display 0.3s , height 0.4s",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  height: "6rem",
+  variants: {
+    show: {
+      true: {
+        display: "flex",
+      },
+      false: {
+        display: "none",
+      },
+    },
+  },
+});
+const Bottom = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  height: "3rem",
+  cursor: "pointer",
+  width: "100%",
+  backgroundColor: "#0E0E0E",
+  transition: "margin 0.4s , height 0.4s",
+  variants: {
+    isOpen: {
+      true: { marginTop: "-18rem", height: "18rem" },
+      false: { height: "3rem" },
+    },
+  },
+});
+
+const PortraitButtonsHeader = styled("div", {
+  display: "flex",
+  width: "100%",
+  maskRepeat: "no-repeat",
+  maskSize: "100% 100%",
+  maskPosition: "bottom",
+  height: "3rem",
+  backgroundColor: "#0E0E0E",
+  maskImage: `url(${albumItemsBg})`,
+  zIndex: 2,
+  variants: {
+    isOpen: {
+      true: { height: "0rem", overflow: "hidden" },
+      false: { height: "3rem" },
+    },
+  },
+});
+
+const Button = styled("button", {
   zIndex: "1",
   background: "#0e0e0e",
-  display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-between",
   alignItems: "center",
   color: "#fff",
   border: "solid 2px #333333",
@@ -57,8 +143,20 @@ const LandscapeButton = styled("button", {
   fontWeight: "bold",
   marginRight: "0.6rem",
   borderRadius: "1.5rem",
+  height: "2.5rem",
   padding: "0.5rem",
   cursor: "pointer",
+  "@landscape": { display: "flex" },
+  variants: {
+    isOpen: {
+      true: {
+        "@portrait": { display: "flex", marginTop: "1rem", width: "90%" },
+      },
+      false: {
+        "@portrait": { display: "none" },
+      },
+    },
+  },
 });
 
 const ButtonText = styled("div", {
